7419de80e802aa7287adfa7a281ac49a69c0cc02
Darius Garcia <dariusgarcia888@gmail.com>
2023-03-20 00:04:06 -0700

    changed backend API url for stockPeers

--- client/src/utils/fetchStockPeers.ts
+++ client/src/utils/fetchStockPeers.ts
@@ -3,11 +3,17 @@ const apiKey = '519b0d38ac484284abb5ed5338c2db0b'
 // fetches Company name, sector, headquarters, dateFirstAdded, founded
 
 export const FetchStockPeers = async (stock: String) => {
-	const BASE_URL = `https://fmpcloud.io/api/v4/stock_peers?symbol=${stock}&apikey=`
-	const api = `${BASE_URL}${apiKey}`
-
-	return await fetch(api)
+	const api = `${process.env.REACT_APP_BACKEND_STOCK_API}peers`
+	const headerOptions = {
+		'Content-Type': 'application/json',
+	  }
+  
+	return await fetch(api, {
+		method: 'POST',
+		headers: headerOptions,
+		body: JSON.stringify({ stock: stock }),
+	  })
 		.then((results) => results.json())
-		.then((data) => data)
+		.then((data) => data?.stockPeers)
 		.catch((err) => console.log(err))
 }
